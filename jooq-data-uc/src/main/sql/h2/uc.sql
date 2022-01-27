4d3d5369c0ea1b5e19dac767c6801d0869212372
liyifei <liyifei@kuaishou.com>
2022-01-27 13:54:18 +0800

    init

--- jooq-data-uc/src/main/sql/h2/uc.sql
+++ jooq-data-uc/src/main/sql/h2/uc.sql
@@ -7,13 +7,13 @@ CREATE TABLE `account`
 (
     `id`            bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '<账户ID>',
     `name`          varchar(255) NOT NULL DEFAULT '' COMMENT '<账户名称>',
-    `user_id`       bigint(20) NOT NULL DEFAULT -1 COMMENT '<用户ID>',
+    `user_id`       bigint(20)   NOT NULL DEFAULT -1 COMMENT '<用户ID>',
     `short_name`    varchar(255) NOT NULL DEFAULT '' COMMENT '<账户简称>',
     `app_id`        int(10)      NOT NULL DEFAULT -1 COMMENT '<产品线>',
     `create_source` int(10)      NOT NULL DEFAULT -1 COMMENT '<业务来源>',
     `product_name`  varchar(255) NOT NULL DEFAULT '' COMMENT '<产品名称>',
     `web_site`      varchar(255) NOT NULL DEFAULT '' COMMENT '<用户网址>',
-    `license_type`  tinyint(4) NOT NULL DEFAULT 1 COMMENT '<个人/企业>',
+    `license_type`  tinyint(4)   NOT NULL DEFAULT 1 COMMENT '<个人/企业>',
     `contact`       varchar(255) NOT NULL DEFAULT '' COMMENT '<联系人>',
     `phone`         varchar(255) NOT NULL DEFAULT '' COMMENT '<电话>',
     `email`         varchar(255) NOT NULL DEFAULT '' COMMENT '<邮箱>',
@@ -23,5 +23,7 @@ CREATE TABLE `account`
     `status`        int(10)      NOT NULL DEFAULT -1 COMMENT '<账户状态>',
     `agent_id`      bigint(20)   NOT NULL DEFAULT -1 COMMENT '<代理商>',
     `shop_user_id`  bigint(20)   NOT NULL DEFAULT -1 COMMENT '<小店用户ID>',
+    `create_time`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time`   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`)
 )/*ENGINE=INNODB DEFAULT CHARSET=UTF8 COMMENT='用户信息表'*/;
